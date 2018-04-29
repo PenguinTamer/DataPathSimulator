2166812162f70cfb8f730287dfd014e1ed4aef70
SpencerGinsburg <sag196@txstate.edu>
2018-04-28 19:22:03 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/vcs.xml

---
+++ app/src/main/java/salazar/cs3354/txstate/edu/datapathsimulator/answer4.java
@@ -0,0 +1,15 @@
+package salazar.cs3354.txstate.edu.datapathsimulator;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class answer4 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer4);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
---
+++ app/src/main/java/salazar/cs3354/txstate/edu/datapathsimulator/answer7.java
@@ -0,0 +1,15 @@
+package salazar.cs3354.txstate.edu.datapathsimulator;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class answer7 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer7);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
