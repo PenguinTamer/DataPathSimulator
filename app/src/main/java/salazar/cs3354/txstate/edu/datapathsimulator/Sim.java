e51b19bac733ba604d0d7b64c692c99cfabba5c3
PenguinTamer <PenguinTamer@users.noreply.github.com>
2018-05-09 16:56:34 -0500

    Logic in Sim started

--- app/src/main/java/salazar/cs3354/txstate/edu/datapathsimulator/Sim.java
+++ app/src/main/java/salazar/cs3354/txstate/edu/datapathsimulator/Sim.java
@@ -3,11 +3,46 @@ package salazar.cs3354.txstate.edu.datapathsimulator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class Sim extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class Sim extends AppCompatActivity {
+    //Handles
+    /***/
+    //Variables
+    /**
+     * Holds all questions (simQuestion objects
+     */
+    private ArrayList<simQuestion> questions;
+
+    /**
+     * Initializes the content view for the activity
+     *
+     * @param savedInstanceState keeps previous states for restoration of activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sim);
+        //Initialize variables
+        questions = new ArrayList<>();
+
+    }
+
+    private void initialize(ArrayList<simQuestion> questions) {
+        simQuestion question;
+
+        //Create new question
+        question = new simQuestion();
+        question.setQuestion("ALU");
+        question.setType('A');
+        question.setqID(1);
+        question.setInfoID(-1);
+        //Add Answers
+        question.answers.add(null);
+        question.answers.add(null);
+        question.answers.add(null);
+        question.answers.add(null);
+        //Commit to array questions
+        questions.add(question);
     }
 }
