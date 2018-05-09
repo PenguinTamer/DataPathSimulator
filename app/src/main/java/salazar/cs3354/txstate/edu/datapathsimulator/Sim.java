12e1b289ad0b60677c7e4512a27d2d408a4efc15
PenguinTamer <PenguinTamer@users.noreply.github.com>
2018-05-09 18:57:19 -0500

    sync push

--- app/src/main/java/salazar/cs3354/txstate/edu/datapathsimulator/Sim.java
+++ app/src/main/java/salazar/cs3354/txstate/edu/datapathsimulator/Sim.java
@@ -2,8 +2,11 @@ package salazar.cs3354.txstate.edu.datapathsimulator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -12,19 +15,35 @@ public class Sim extends AppCompatActivity {
     /**Handle for dropdown containing choices for spinner 1 of row 1 in answer table*/
     /**Handle for dropdown containing choices for spinner 2 of row 1 in answer table*/
     /**Handle for dropdown containing choices for spinner 1 of row 2 in answer table*/
+    /**Handle for dropdown containing choices for spinner 2 of row 2 in answer table*/
+    /**Handle for dropdown containing choices for spinner 1 of row 3 in answer table*/
+    /**Handle for dropdown containing choices for spinner 2 of row 3 in answer table*/
+    /**Handle for dropdown containing choices for spinner 1 of row 4 in answer table*/
+    /**Handle for dropdown containing choices for spinner 2 of row 4 in answer table*/
+    /**Handle for dropdown containing choices for spinner 1 of row 5 in answer table*/
+    /**Handle for dropdown containing choices for spinner 2 of row 5 in answer table*/
+    /**Handle for dropdown containing choices for spinner 1 of row 6 in answer table*/
+    /**Handle for dropdown containing choices for spinner 2 of row 6 in answer table*/
+    /**Handle for dropdown containing choices for spinner 1 of row 7 in answer table*/
     /**
-     * Handle for dropdown containing choices for spinner 2 of row 2 in answer table
+     * Handle for dropdown containing choices for spinner 2 of row 7 in answer table
      */
-    private Spinner r1spinner1, r1spinner2, r2spinner1, r2spinner2;
+    private Spinner r1spinner1, r1spinner2, r2spinner1, r2spinner2, r3spinner1, r3spinner2,
+            r4spinner1, r4spinner2, r5spinner1, r5spinner2, r6spinner1, r6spinner2, r7spinner1,
+            r7spinner2;
     /**
      * Handle for the submit button that submits answers entered into answer table
      */
     private Button submitButton;
+    /**
+     * Handle for the title of sim activity
+     */
+    private TextView instType;
     //Variables
     /**
-     * Holds all questions (simQuestion objects
+     * Holds question (simQuestion object)
      */
-    private ArrayList<simQuestion> questions;
+    private simQuestion question;
 
     /**
      * Initializes the content view for the activity
@@ -34,30 +53,119 @@ public class Sim extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sim);
+
+        //Initialize handles
+        r1spinner1 = findViewById(R.id.r1spinner1);
+        r1spinner2 = findViewById(R.id.r1spinner2);
+        r2spinner1 = findViewById(R.id.r2spinner1);
+        r2spinner2 = findViewById(R.id.r2spinner2);
+        r3spinner1 = findViewById(R.id.r3spinner1);
+        r3spinner2 = findViewById(R.id.r3spinner2);
+        r4spinner1 = findViewById(R.id.r4spinner1);
+        r4spinner2 = findViewById(R.id.r4spinner2);
+        r5spinner1 = findViewById(R.id.r5spinner1);
+        r5spinner2 = findViewById(R.id.r5spinner2);
+        r6spinner1 = findViewById(R.id.r6spinner1);
+        r6spinner2 = findViewById(R.id.r6spinner2);
+        r7spinner1 = findViewById(R.id.r7spinner1);
+        r7spinner2 = findViewById(R.id.r7spinner2);
         //Initialize variables
-        questions = new ArrayList<>();
+        question = initialize(getIntent().getExtras().getChar("type"));
 
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkAnswers(question, getAnswers())) {
+                    Toast.makeText(getApplicationContext(), "You got it right!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "You got it wrong!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-    private void initialize(ArrayList<simQuestion> questions) {
+    private simQuestion initialize(char type) {
         simQuestion question;
-
-        //Create new question
-        question = new simQuestion();
-        question.setQuestion("ALU");
-        question.setType('A');
-        question.setqID(1);
-        question.setInfoID(-1);
-        //Add Answers
-        question.answers.add(null);
-        question.answers.add(null);
-        question.answers.add(null);
-        question.answers.add(null);
-        //Commit to array questions
-        questions.add(question);
+        switch (type) {
+            case 'A':
+            case 'a':
+                //Create new question
+                question = new simQuestion();
+                question.setQuestion("ALU Instruction");
+                question.setType('A');
+                question.setqID(-1);
+                question.setInfoID(-1);
+                //Add Answers
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                break;
+            case 'L':
+            case 'l':
+                //Create new question
+                question = new simQuestion();
+                question.setQuestion("Load Instruction");
+                question.setType('L');
+                question.setqID(-1);
+                question.setInfoID(-1);
+                //Add Answers
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                break;
+            case 'S':
+            case 's':
+                //Create new question
+                question = new simQuestion();
+                question.setQuestion("Store Instruction");
+                question.setType('S');
+                question.setqID(-1);
+                question.setInfoID(-1);
+                //Add Answers
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                question.answers.add(null);
+                break;
+            default:
+                question = new simQuestion();
+        }
+        return question;
     }
 
-    private void checkAnswers() {
+    private ArrayList getAnswers() {
+        ArrayList<Integer> userAnswers = new ArrayList<>();
+        userAnswers.add(Integer.valueOf(r1spinner1.getSelectedItem().toString().concat(r1spinner2.getSelectedItem().toString())));
+        userAnswers.add(Integer.valueOf(r2spinner1.getSelectedItem().toString().concat(r2spinner2.getSelectedItem().toString())));
+        userAnswers.add(Integer.valueOf(r3spinner1.getSelectedItem().toString().concat(r3spinner2.getSelectedItem().toString())));
+        userAnswers.add(Integer.valueOf(r4spinner1.getSelectedItem().toString().concat(r4spinner2.getSelectedItem().toString())));
+        userAnswers.add(Integer.valueOf(r5spinner1.getSelectedItem().toString().concat(r5spinner2.getSelectedItem().toString())));
+        userAnswers.add(Integer.valueOf(r6spinner1.getSelectedItem().toString().concat(r6spinner2.getSelectedItem().toString())));
+        userAnswers.add(Integer.valueOf(r7spinner1.getSelectedItem().toString().concat(r7spinner2.getSelectedItem().toString())));
+        return userAnswers;
+    }
 
+    private boolean checkAnswers(simQuestion question, ArrayList<Integer> userAnswers) {
+        for (int i = 0; i < question.answers.size(); i++)
+            for (int j = 0; j < userAnswers.size(); j++) {
+                if (question.answers.get(i) != userAnswers.get(j)) {
+                    return false;
+                }
+            }
+        return true;
     }
 }
