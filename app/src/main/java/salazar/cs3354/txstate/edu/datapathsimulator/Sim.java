d9c955dfc78a0cea0d38c17ad334503d765976ac
PenguinTamer <PenguinTamer@users.noreply.github.com>
2018-05-09 17:23:28 -0500

    Commit

--- app/src/main/java/salazar/cs3354/txstate/edu/datapathsimulator/Sim.java
+++ app/src/main/java/salazar/cs3354/txstate/edu/datapathsimulator/Sim.java
@@ -2,12 +2,24 @@ package salazar.cs3354.txstate.edu.datapathsimulator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class Sim extends AppCompatActivity {
     //Handles
-    /***/
+    /**Handle for dropdown containing choices for spinner 1 of row 1 in answer table*/
+    /**Handle for dropdown containing choices for spinner 2 of row 1 in answer table*/
+    /**Handle for dropdown containing choices for spinner 1 of row 2 in answer table*/
+    /**
+     * Handle for dropdown containing choices for spinner 2 of row 2 in answer table
+     */
+    private Spinner r1spinner1, r1spinner2, r2spinner1, r2spinner2;
+    /**
+     * Handle for the submit button that submits answers entered into answer table
+     */
+    private Button submitButton;
     //Variables
     /**
      * Holds all questions (simQuestion objects
@@ -16,7 +28,6 @@ public class Sim extends AppCompatActivity {
 
     /**
      * Initializes the content view for the activity
-     *
      * @param savedInstanceState keeps previous states for restoration of activity
      */
     @Override
@@ -44,5 +55,9 @@ public class Sim extends AppCompatActivity {
         question.answers.add(null);
         //Commit to array questions
         questions.add(question);
+    }
+
+    private void checkAnswers() {
+
     }
 }
